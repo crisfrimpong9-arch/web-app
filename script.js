3dcfbe8560cbedbd3e66e9f8966a9774d4e27a3b
Christian Frimpong <crisfrimpong9@gmail.com>
2025-08-12 22:35:56 +0000

    Added starter HTML, CSS, and JS files

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("My web app is running!");
-
 document.getElementById("clickBtn").addEventListener("click", function() {
-    alert("You clicked the button! 🎉");
+    alert("Hello Christian! 🚀 Your web app is working.");
 });
